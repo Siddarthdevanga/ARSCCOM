be2b930d6b00affd258892f803438f3043faeddc
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-22 01:34:12 +0530

    Update page.js

--- frontend/app/reset_password/page.js
+++ frontend/app/reset_password/page.js
@@ -42,7 +42,7 @@ function ResetPasswordContent() {
     /* ================= API ================= */
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/reset_password`,
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/reset_password`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
