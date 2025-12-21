737b2435d8891d8a509e11a58b9a9034889e943e
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-21 22:45:38 +0530

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,14 @@
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default function Page() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token");
+
+  if (token) {
+    redirect("/home");
+  }
+
   redirect("/auth/login");
 }
+
