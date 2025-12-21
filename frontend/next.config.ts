60e2b8111a27d517cc0393d88679ab2eae8d8ce8
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-21 22:59:46 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/login",
+        destination: "/auth/login"
+      },
+      {
+        source: "/forgot-password",
+        destination: "/auth/forgot-password"
+      }
+    ];
+  }
 };
 
 export default nextConfig;
