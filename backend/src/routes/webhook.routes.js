9192fbdfaf9e87dcb1129e5af37e21476be748a2
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-27 12:50:25 +0530

    Delete backend/src/routes/webhook.routes.js

--- backend/src/routes/webhook.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { zohoWebhook } from "../controllers/webhook.controller.js";
-
-const router = express.Router();
-router.post("/zoho", zohoWebhook);
-
-export default router;
