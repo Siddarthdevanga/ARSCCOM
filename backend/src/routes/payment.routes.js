0d9132891c0b1df47c4f1e62ae573acc2b7a3e5c
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-27 12:50:01 +0530

    Delete backend/src/routes/payment.routes.js

--- backend/src/routes/payment.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createPaymentLink } from "../controllers/payment.controller.js";
-import { authMiddleware } from "../middleware/auth.js";
-
-const router = express.Router();
-
-router.post("/pay", authMiddleware, createPaymentLink);
-
-export default router;
