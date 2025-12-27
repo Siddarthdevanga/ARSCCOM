78756eb91ffa79c552c799146f316d1f63de31a4
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-27 14:30:11 +0530

    Delete backend/src/routes/billing.routes.js

--- backend/src/routes/billing.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { authenticate } from "../middlewares/auth.middleware.js";
-import { createPayment } from "../controllers/billing.controller.js";
-
-const router = express.Router();
-
-router.post("/pay", authenticate, createPayment);
-
-export default router;
