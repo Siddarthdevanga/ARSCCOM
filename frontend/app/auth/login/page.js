32dd813bdcd06dadf2806e5829fd791844071554
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2026-01-02 11:39:04 +0530

    Update page.js

--- frontend/app/auth/login/page.js
+++ frontend/app/auth/login/page.js
@@ -113,7 +113,7 @@ export default function LoginPage() {
           </div>
 
           <Image
-            src="/promeet.png"
+            src="/logo.png"
             alt="promeet"
             width={420}
             height={140}
