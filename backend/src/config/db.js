4d5d980ce2132378ea32ce79684234b3c50ceb69
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2026-01-22 17:31:12 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,6 +6,6 @@ export const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 1000,
   queueLimit: 0
 });
